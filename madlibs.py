0938252cb5c74c26ff54ed1eb72dee266575819e
Otter03Black <otterblack03@gmail.com>
2023-04-12 17:01:20 -0500

    Last update for madlibs

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+#Mad libs is a game where you have to tell me your story in some key words about how and when did you earn your first 5 dollar bill. First, I will tell you my Story.
+
+mymadlib = f"Mad libs is a game where you have to tell me your story in some key words about how and when did you earn your first five dollars. I will start telling you my Story.'!Yesss¡' It was my expression the day when I earned my frist five dollars. I was cleaning my aunt yard and because of my great job she gave me five dollars. After that, I decided to buy my first headphones... I was really proud."
+print(mymadlib)
+
+str1 = input("How was your Story?")
+str2 = input("Where were you at?")
+str3 = input("What were you doing for earning your first five dollars?")
+integer = int(input("How old were you?"))
+str4 = input("Do you remember the wheather of this day?")
+
+usermadlib = f"How was your story?: {str1} Where were you at?: {str2} What were you doing for earning your first five dollars?: {str3} How old were you?: {integer} Do you remember the wheather of this day?: {str4}..."
+print(usermadlib)
+
+
+

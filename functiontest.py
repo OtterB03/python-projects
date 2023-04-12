8e714a56cd6d3698eab59a499c58c8e1669f9f02
Otter03Black <otterblack03@gmail.com>
2023-04-12 17:02:55 -0500

    last of functiontest and guessthenumber

---
+++ functiontest.py
@@ -0,0 +1,11 @@
+def my_function(deff, self, raton):
+    print(str(deff) + str(self) + " " + str(raton)) 
+
+
+add = int(input(" Insert a number and I'll add a 10 for you"))
+calling = " Emerus"
+fname = " Hok"
+
+my_function(add, calling, fname)
+
+
--- guessthenumber.py
+++ guessthenumber.py
@@ -1,3 +1,17 @@
 
-def hangman():
-    list
+def hangman(x): 
+    list = [1, 2, "Hello", "Murder"]
+hangman(1)
+
+
+def firulais(x):
+    list = [1, 5, "Dev"]
+    print(list[x])
+firulais(2)
+
+
+def addition():
+    add = int(input("Insert a number and I'll add a 10 for you")) + 10
+    print(add) 
+addition()
+

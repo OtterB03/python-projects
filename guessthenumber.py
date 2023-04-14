0b54fd72757a443ac8c8eba26644e61da3fa26d2
Otter03Black <otterblack03@gmail.com>
2023-04-14 09:47:39 -0500

    Lasta Updat April 14 9:47. Finished number of chances with stop on the 0 chance.

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,17 +1,58 @@
+# Guess the Numer is a game where you have been given a numbers range, then you have type a number in this range. After that, cold, hot or correct alerts will appear telling you how close or far to the correct number you are... Have fun Guessing!
 
-def hangman(x): 
-    list = [1, 2, "Hello", "Murder"]
-hangman(1)
+def guessnum():
+    explaning = f"Find the correct number between 1 and 10. You have 5 trys."
+    print(explaning)
+    correct_num = 5
+    user_input = 0
+    trys_counter = 5
+    while user_input != correct_num:
+        try: 
+            user_input = int(input("Type a number between 1 and 10: "))
+            
+        except:
+            if trys_counter == 1:
+                print("Thanks for playing")
+                break
 
+            print("InvalidCharacterError")
 
-def firulais(x):
-    list = [1, 5, "Dev"]
-    print(list[x])
-firulais(2)
+            trys_counter -= 1
+            if trys_counter == 1:
+                print("Last chance")
+    
+            print(f"Next try. {trys_counter} remaning")
+            
+                
 
+            continue #If "Invalid Character appears, game will continue until trys_counter will be 0"
 
-def addition():
-    add = int(input("Insert a number and I'll add a 10 for you")) + 10
-    print(add) 
-addition()
+        if user_input == 5:
+            print("Correct!")
+            print("Thanks for playing")
+        elif user_input <= 3 or user_input >= 7:
+            if trys_counter == 1:
+                print("Thanks for playing")
+                break
 
+            print("Cold!")
+
+            trys_counter -= 1
+            if trys_counter == 1:
+                print("Last chance")
+    
+            print(f"Next try. {trys_counter} remaning")
+        elif user_input == 4 or user_input == 6:
+            if trys_counter == 1:
+                print("Thanks for playing")
+                break
+
+            print("warm!")
+
+            trys_counter -= 1
+            if trys_counter == 1:
+                print("Last chance")
+    
+            print(f"Next try. {trys_counter} remaning")
+
+guessnum()
